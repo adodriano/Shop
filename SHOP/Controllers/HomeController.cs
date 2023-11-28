833b843f724f3cd9b6ce1b52d6db59ac9e89ddb4
adodriano <adodriano@gmail.com>
2023-11-27 22:12:42 -0300

    Ajuste API

--- SHOP/Controllers/HomeController.cs
+++ SHOP/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SHOP.Data;
+using SHOP.Models;
 
 namespace SHOP.Controllers
 {
@@ -7,6 +9,23 @@ namespace SHOP.Controllers
     [ApiController]
     public class HomeController : Controller
     {
+        public async Task<ActionResult<dynamic>> Get([FromServices] DataContext context )
+        {
+            var employee = new User {Id = 1, UserName = "pedro", Password = "pedro", Role = "employee"};
+            var manager = new User {Id = 2, UserName = "maria", Password = "maria", Role = "employee"};
+            var category = new Category {Id = 1, Title = "Informática"};
+            var product = new Product {Id = 1, Category = category, Title = "Mouse", Price = 299, Description = "Mouse sem fio"};
+            context.Users.Add(employee);
+            context.Users.Add(manager); 
+            context.Categories.Add(category);
+            context.Products.Add(product);
+            await context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                message = "Dados configurados"
+            });
+        }
 
     }
 }
